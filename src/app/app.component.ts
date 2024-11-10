7220c8707fe98edb4e331b723cafa4e0e292bd45
Christian Fuentes <christian.fuenteszelada@gmail.com>
2024-11-10 12:24:12 -0300

    Antes de crear los modulos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "./sidebar/sidebar.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'gifsApp';
+  isCollapsed: boolean = true;
 }
